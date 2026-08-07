f9d9e87b1ed48aabe6db72cefcfd5e4ccf2d5210
zainabahmed <zainabahmedlll19@gmail.com>
2026-08-07 16:23:18 +0300

    edit user's bio & profilepic

--- src/pages/EditProfile.jsx
+++ src/pages/EditProfile.jsx
@@ -1,9 +1,66 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { show, update } from "../services/user";
 import { useParams, useNavigate } from "react-router";
 
 
-const EditProfile = () => {
+const EditProfile = (props) => {
      const { userId } = useParams()
      const navigate = useNavigate()
+     const [message, setMessage] = useState('')
+     const [profilePic, setProfilePic]= useState(null)
+     const [formData, setFormData] = useState({ bio: ''})
+
+    useEffect(() => {
+             const fetchUser = async () => {
+                 const userData = await show(userId)
+                 setFormData({bio: userData.bio || ''})
+             }
+             fetchUser()
+         }, [userId])
+
+    const handleFileChange = (event) => {
+        setProfilePic(event.target.files[0])
+    }
+
+    const handleChange = (event) => {
+        setFormData({...formData, [event.target.name]: event.target.value})
+    }
+
+    const handleSubmit = async (event) => {
+        event.preventDefault()
+        try {
+            const submitData = new FormData()
+            submitData.append('bio', formData.bio)
+            if (profilePic) {
+                submitData.append('profilePic', profilePic)
+            }
+            
+            await update(userId, submitData)
+            navigate(`/users/${userId}`)
+        } catch (err) {
+            setMessage(err.message)
+        }
+    }
+
+   
+
+    return (
+        <section>
+            <header>
+                <h1>Edit profile</h1>
+            </header>
+            <form onSubmit={handleSubmit}>
+                Bio:
+                <textarea name="bio" value={formData.bio} onChange={handleChange} />
+                Profile Pic:
+                <input type="file" name="profilePic" onChange={handleFileChange} />
+
+                <button type="submit">Submit</button>
+                <button type="button"onClick={() => navigate(`/users/${userId}`)} >Cancel</button>
+            </form>
+        </section>
+    )
+
 }
+
+export default EditProfile
