76ed9ba6db079737e010550f66541408602afcef
fatimaAhmed26 <fatima.bh2248@gmail.com>
2026-08-09 10:01:48 +0300

    create function

--- src/services/post.js
+++ src/services/post.js
@@ -12,7 +12,23 @@ const index = async () => {
   }
 }
 
+const create = async (postFormData) => {
+  try {
+    const res = await fetch(BASE_URL, {
+      method: 'POST',
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem('token')}`,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(postFormData),
+    })
+    return res.json()
+  } catch (error) {
+    console.log(error)
+  }
+}
 export{
     index,
-    
+    create,
+
 }
