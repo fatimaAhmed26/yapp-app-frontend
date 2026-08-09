300ab4d1de768e69f40e50fc0a17c2039fef174e
fatimaAhmed26 <fatima.bh2248@gmail.com>
2026-08-09 10:33:45 +0300

    adding the route for the post and the form page

---
+++ src/pages/PostFrom.jsx
@@ -0,0 +1,12 @@
+import { useState, useEffect } from 'react'
+import { useParams } from 'react-router'
+import * as postServices from '../services/post'
+
+const PostForm =(post)=>{
+    return(
+        <dev>
+            <h1>post form</h1>
+        </dev>
+    )
+}
+export default PostForm
