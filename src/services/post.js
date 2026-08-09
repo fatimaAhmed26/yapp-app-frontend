d93436b05981095d9be50946cba6415e2b7b58ee
fatimaAhmed26 <fatima.bh2248@gmail.com>
2026-08-09 10:04:34 +0300

    update function

--- src/services/post.js
+++ src/services/post.js
@@ -41,9 +41,28 @@ const deletePost = async (postId) => {
     console.log(error)
   }
 }
+
+async function update(postId, postFormData) {
+  try {
+    const res = await fetch(`${BASE_URL}/${postId}`, {
+      method: 'PUT',
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem('token')}`,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(postFormData),
+    })
+    return res.json()
+  } catch (error) {
+    console.log(error)
+  }
+}
+
 export{
     index,
     create,
     deletePost,
+    update,
+    
 
 }
