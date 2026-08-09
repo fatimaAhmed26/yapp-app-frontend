d894df1da3b3dda8bdfb75afa0e8c351d927bc4f
fatimaAhmed26 <fatima.bh2248@gmail.com>
2026-08-09 11:42:44 +0300

    adding handlers for the form data

--- src/pages/PostFrom.jsx
+++ src/pages/PostFrom.jsx
@@ -1,11 +1,44 @@
 import { useState, useEffect } from 'react'
-import { useParams } from 'react-router'
+import { useActionData, useParams } from 'react-router'
 import * as postServices from '../services/post'
 
-const PostForm =(post)=>{
+const PostForm =(props)=>{
+    const { postId } = useParams()
+    const initialState ={
+        media:'',
+        text:''
+    }
+    const [formData , setFormData] = useState(initialState)
+    const [appMedia ,setAppMedia] =useState('')
+    const handleSubmit = (event)=>{
+        evt.preventDefault()
+        props.handleAddPost(formData)
+    }
+    const handleChange=(evt)=>{
+        setFormData({ ...formData, [evt.target.name]: evt.target.value })
+    }
+     const handleFileChange = (event) => {
+        setAppMedia(event.target.files[0])
+    }
+    
+  useEffect(() => {
+    const fetchPost = async () => {
+        const postData = await postServices.show(postId)
+        setFormData(postData)
+    }
+    if (postId) fetchPost()
+    
+    return () => setFormData(initialState)
+  }, [postId])
     return(
         <dev>
             <h1>post form</h1>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="media">Share your thoughts by adding a text, image or video </label>
+                <input type="file" name='media' onChange={handleFileChange} />
+                <input type="text" name='text' onChange={handleChange}/>
+                <button type='submit'>Add Post</button>
+            </form>
         </dev>
     )
 }
