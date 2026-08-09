2abd29509ece459f93257c2f059b7586e1b12abf
fatimaAhmed26 <fatima.bh2248@gmail.com>
2026-08-09 12:13:36 +0300

    adding the /posts route and the post list page

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import EditProfile from "./pages/EditProfile"
 import UserList from "./pages/UserList"
 import * as postService from './services/post'
 import PostForm from "./pages/PostFrom"
+import PostList from "./pages/PostList"
 
 const getUserFromToken = () => {
   const token = localStorage.getItem('token')
@@ -42,6 +43,8 @@ const [posts ,SetPosts] = useState()
         <Route path="/users/:userId/edit" element={<EditProfile user={user} />} />
         <Route path="/users/:userId/followers" element={<UserList type='followers' /> } />
         <Route path="/users/:userId/following" element={<UserList type='following' /> } />
+
+        <Route path="/Posts" element={<PostList posts={posts}/>}/>
         <Route path='/posts/new' element={<PostForm handleAddPost={handleAddPost}/>}/>
       </Routes>
       </main>
---
+++ src/pages/PostList.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router"
+
+const PostList =()=>{
+
+    return(
+        <main>
+            <h1>post list</h1>
+        </main>
+    )
+
+}
+export default PostList
