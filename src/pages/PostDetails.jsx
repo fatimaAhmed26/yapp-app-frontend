2f52194d88b85f287488c6ab19071f556f621600
fatimaAhmed26 <fatima.bh2248@gmail.com>
2026-08-10 13:03:48 +0300

    adding the delete function in posts

--- src/pages/PostDetails.jsx
+++ src/pages/PostDetails.jsx
@@ -1,8 +1,14 @@
 import { useParams, useNavigate, Link } from "react-router"
+import { useRef } from 'react';
 const PostDetails = (props) =>{
       const { postId } = useParams()
   const navigate = useNavigate()
   const post = props.posts.find((post) => post._id === postId)
+    
+
+      const dialogRef = useRef(null);
+    const openModal = () => dialogRef.current?.showModal();
+      const closeModal = () => dialogRef.current?.close();
 
 return(
     <main>
@@ -14,6 +20,13 @@ return(
     {post.media?.type === 'video' && (
       <video src={post.media.url} controls width="300" />
     )}
+      <button onClick={openModal}>Delete</button>
+      <dialog ref={dialogRef} style={{ padding: '20px', borderRadius: '8px', border: 'none' }}>
+        <h2>Are you sure?</h2>
+        <p>when you click delete this post will be deleted</p>
+                    <button onClick={() => props.deletePost(postId)}>Delete</button>
+        <button onClick={closeModal}>Close</button>
+      </dialog>
 
     </main>
 )
