d123dce288f758a1055eb1856b5630bfb2aa150f
fatimaAhmed26 <fatima.bh2248@gmail.com>
2026-08-11 09:12:03 +0300

    commit

--- src/pages/PostDetails.jsx
+++ src/pages/PostDetails.jsx
@@ -1,14 +1,8 @@
 import { useParams, useNavigate, Link } from "react-router"
-import { useRef } from 'react';
 const PostDetails = (props) =>{
       const { postId } = useParams()
   const navigate = useNavigate()
   const post = props.posts.find((post) => post._id === postId)
-    
-
-      const dialogRef = useRef(null);
-    const openModal = () => dialogRef.current?.showModal();
-      const closeModal = () => dialogRef.current?.close();
 
 return(
     <main>
@@ -20,13 +14,6 @@ return(
     {post.media?.type === 'video' && (
       <video src={post.media.url} controls width="300" />
     )}
-      <button onClick={openModal}>Delete</button>
-      <dialog ref={dialogRef} style={{ padding: '20px', borderRadius: '8px', border: 'none' }}>
-        <h2>Are you sure?</h2>
-        <p>when you click delete this post will be deleted</p>
-                    <button onClick={() => props.deletePost(postId)}>Delete</button>
-        <button onClick={closeModal}>Close</button>
-      </dialog>
 
     </main>
 )
