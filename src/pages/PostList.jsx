d3edd3dfd916de67a11bbfd7fd6518e9315deb4a
fatimaAhmed26 <fatima.bh2248@gmail.com>
2026-08-12 10:26:48 +0300

    adding style to the post list

--- src/pages/PostList.jsx
+++ src/pages/PostList.jsx
@@ -1,5 +1,7 @@
 import { Link } from "react-router"
 import { likeToggle } from "../services/post"
+import { HeartIcon } from "@heroicons/react/24/outline"
+import { HeartIcon as HeartIconSolid } from "@heroicons/react/24/solid"
 
 const PostList = (props) => {
     const handleLike = async (postId) => {
@@ -8,30 +10,48 @@ const PostList = (props) => {
     }
 
     return (
-        <main >  
-            {props.posts.map((post) => {
-                const isLiked = props.user && post.likes?.some((like) => like === props.user._id || like._id === props.user._id)
-                const isOwnPost = props.user && props.user._id === post.owner
+        <main className="feed">
+            <ul className="feed-list">
+                {props.posts.map((post) => {
+                    const isLiked = props.user && post.likes?.some((like) => like === props.user._id || like._id === props.user._id)
 
-                return (
-                    <div key={post._id} >
-                        <Link to={`/posts/${post._id}`}>
-                            {post.text && <p>{post.text}</p>}
-                            {post.media?.type === 'image' && (
-                                <img src={post.media.url} alt="media" width="300" />
-                            )}
-                            {post.media?.type === 'video' && (
-                                <video src={post.media.url} controls width="300" />
-                            )}
-                          
-                        </Link>
+                    return (
+                        <li className="feed-post" key={post._id}>
+                            <Link to={`/users/${post.owner?._id}`} className="feed-post-owner">
+                                {post.owner?.profilePic ? (
+                                    <img className="feed-avatar" src={post.owner.profilePic} alt={post.owner.username} />
+                                ) : (
+                                    <div className="feed-avatar-placeholder"></div>
+                                )}
+                                <span className="feed-username">{post.owner?.username}</span>
+                            </Link>
 
-                        <button onClick={() => handleLike(post._id)}>
-                            {isLiked ? 'Unlike' : 'Like'} ({post.likes?.length || 0})
-                        </button>
-                    </div>
-                )
-            })}
+                            <Link to={`/posts/${post._id}`} className="feed-post-link">
+                                {post.text && <p className="feed-post-text">{post.text}</p>}
+                                {post.media?.type === 'image' && (
+                                    <div className="feed-post-media">
+                                        <img src={post.media.url} alt="media" />
+                                    </div>
+                                )}
+                                {post.media?.type === 'video' && (
+                                    <div className="feed-post-media">
+                                        <video src={post.media.url} controls />
+                                    </div>
+                                )}
+                            </Link>
+
+                            <button className="feed-like-btn" onClick={() => handleLike(post._id)}>
+                                {isLiked ? (
+                                    <HeartIconSolid className="like-icon like-icon-active" />
+                                ) : (
+                                    <HeartIcon className="like-icon" />
+                                )}
+                                {post.likes?.length || 0}
+                            </button>
+                        </li>
+                    )
+                })}
+            </ul>
         </main>
     )
 }
