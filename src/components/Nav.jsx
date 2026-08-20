987301a103f0eea899d1729e5c283e3277a16dcc
zainabahmed <zainabahmedlll19@gmail.com>
2026-08-20 18:33:30 +0300

    nav css mobile

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -14,47 +14,46 @@ const Nav = (props) => {
             Yapp
             </Link> */}
             { props.user ? (
-                <ul>
-                    
-                    <li>
-                        <Link to="/posts">
-                        <HomeIcon className="nav-icon" />
-                        Home</Link>
-                    </li>
-                    <li>
-                    <Link to={`/users/${props.user._id}`}>
-                    <UserIcon className="nav-icon" />
-                    Profile</Link>
-                </li>
-                <li>
-                   <Link to={`/users/${props.user._id}/followers`}>
-                   <UserGroupIcon className="nav-icon" />
-                   Followers
-                    </Link>
-                    </li>
-                     <li>
-                   <Link to={`/users/${props.user._id}/following`}>
-                   <UsersIcon className="nav-icon" />
-                   Following
-                    </Link>
-                    </li>
-                     <li>
-                    <a onClick={props.isDarkMode ? props.handleLight : props.handleDark} style={{ cursor: 'pointer' }}>
-                    {props.isDarkMode ? <SunIcon className="nav-icon" /> : <MoonIcon className="nav-icon" />}
-                    {props.isDarkMode ? 'Light Mode' : 'Dark Mode'}
-                     </a>
-                    </li>
-                    <li>
-                        <Link to='/posts/new'>
-                        <PlusCircleIcon className="nav-icon" />
-                        Add post</Link>
-                    </li>
-                    <li>
-                        <Link to="/" onClick={handleSignOut}>
-                        <ArrowRightStartOnRectangleIcon className="nav-icon" />
-                        Sign Out</Link>
-                    </li>
-                </ul>
+                 <ul>
+    <li className="nav-home">
+        <Link to="/posts">
+        <HomeIcon className="nav-icon" />
+        Home</Link>
+    </li>
+    <li className="nav-profile">
+    <Link to={`/users/${props.user._id}`}>
+    <UserIcon className="nav-icon" />
+    Profile</Link>
+</li>
+<li className="nav-desktop-only">
+   <Link to={`/users/${props.user._id}/followers`}>
+   <UserGroupIcon className="nav-icon" />
+   Followers
+    </Link>
+    </li>
+     <li className="nav-desktop-only">
+   <Link to={`/users/${props.user._id}/following`}>
+   <UsersIcon className="nav-icon" />
+   Following
+    </Link>
+    </li>
+     <li className="nav-darkmode">
+    <a onClick={props.isDarkMode ? props.handleLight : props.handleDark} style={{ cursor: 'pointer' }}>
+    {props.isDarkMode ? <SunIcon className="nav-icon" /> : <MoonIcon className="nav-icon" />}
+    {props.isDarkMode ? 'Light Mode' : 'Dark Mode'}
+     </a>
+    </li>
+    <li className="nav-add-post">
+        <Link to='/posts/new'>
+        <PlusCircleIcon className="nav-icon" />
+        Add post</Link>
+    </li>
+    <li className="nav-signout">
+        <Link to="/" onClick={handleSignOut}>
+        <ArrowRightStartOnRectangleIcon className="nav-icon" />
+        Sign Out</Link>
+    </li>
+</ul>
                 
             ) : (
             <ul>
